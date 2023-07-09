b7275812ce75c5819aa9c9812f421c08e37525b8
marcos <marco.quichimbo@epn.edu.ec>
2023-07-09 15:22:18 -0500

    hasta correr por primera vez
    ejecucion del.kt exitosa

--- 00-kotlin/Moviles/src/main/kotlin/Main.kt
+++ 00-kotlin/Moviles/src/main/kotlin/Main.kt
@@ -61,6 +61,18 @@ fun main(args: Array<String>) {
     calcularSueldo(10.00, bonoEspecial = 20.00)
     calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00) //no importa el orden
 
+    val sumaUno = Suma(1,1)
+    val sumaDos = Suma(null, 1)
+    val sumaTres = Suma(1,null)
+    val sumaCuatro = Suma(null, null)
+    sumaUno.sumar()
+    sumaDos.sumar()
+    sumaTres.sumar()
+    sumaCuatro.sumar()
+    println(Suma.pi)
+    println(Suma.elevarCuadrado(2))
+    println(Suma.historialSumas)
+
 }
 //void -> unit
 fun imprimirNombre(nombre: String): Unit{
@@ -116,3 +128,76 @@ abstract class Numeros(//constructor PRIMARIO
     }
 
 }
+
+class Suma( //Constructor primario Suma
+    uno: Int, //parametro
+    dos: Int //parametro
+): Numeros(uno, dos){ // <- este es el constructor del padre
+    init { //bloque construcor primario
+        this.numeroUno; numeroUno
+        this.numeroDos; numeroDos
+    }
+    constructor(//segundo constructor
+        uno:Int?, //parametros
+        dos:Int //parametros
+    ): this(//llamada al constructor primario
+        if(uno == null) 0 else uno,
+        dos
+       ){//si necesitamos un bloque de codigo lo usamos
+            numeroUno
+         }
+
+    constructor(//tercer constructor
+        uno:Int, //parametros
+        dos:Int? //parametros)
+    ): this( //llamada al constructor primario
+        uno,
+        if (dos == null) 0 else uno
+    )// si no necesitamos al bloque de codigo lo omitimos {}
+
+    constructor(//cuarto constructor
+        uno: Int?, //parametros
+        dos: Int?//parametros
+    ): this( //llamada al constructor primario
+        if (uno == null) 0 else uno,
+        if (dos == null) 0 else uno
+    )
+
+    public fun sumar(): Int{ //public por defecto, o usar private o protected
+        val total = numeroUno + numeroDos
+
+        agregarHistorial(total)
+        return total
+    }
+
+    companion object { //atributos y metodos compartidos
+        //entre las instancias
+        val pi = 3.14
+        fun elevarCuadrado(num: Int): Int{
+            return num * num
+        }
+        val historialSumas = arrayListOf<Int>()
+        fun agregarHistorial(valorNuevaSuma: Int){
+            historialSumas.add(valorNuevaSuma)
+        }
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
