d256d7a4c69501f63e4054f9af0fbdeba22c5e63
marcos <marco.quichimbo@epn.edu.ec>
2023-07-14 22:08:05 -0500

    hasta abrirconparametros

--- 01-android-v4/app/src/main/java/com/example/a01_android_v5/MainActivity.kt
+++ 01-android-v4/app/src/main/java/com/example/a01_android_v5/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener {
                 irActividad(BListView::class.java)
             }
-    }
+    }git
     fun irActividad(
         clase: Class <*>
     ){
